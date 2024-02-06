772aebe96a4d31fe1eaf161d0752ef18eb38b193
TheSuspecct <baogiakhuong2005@gmail.com>
2024-02-06 16:25:54 -0500

    Update game.py
    added evaluate, minimax algorithm, generate_possible_moves functions

--- game.py
+++ game.py
@@ -25,11 +25,55 @@ class TicTacToe:
     def full(self, board):
         return np.all(board != '-')
     
-    def evaluate(self, board):
-        pass
+    def evaluate(self, board, maximizing):
+        if self.winner(board):
+            if maximizing == True:
+                return 1
+            return -1
 
-    def minimax(self, position, depth, alpha, beta, maximizing):
-        if depth == 0 or self.winner(position) == True or self.full(position) == True:
-            return self.evaluate(position)
-    
+        elif self.full(board):
+            return 0
+
+    def generate_possible_moves(self, board, player):
+        possible_moves = []
+
+        for row in range(3):
+            for col in range(3):
+                if board[row][col] == "-":
+                    newboard = np.copy(board)
+                    newboard[row, col] = player
+                    possible_moves.append(newboard)
+        return possible_moves
+
+    def minimax(self, board, depth, alpha, beta, maximizing):
+        if depth == 0 or self.winner(board) == True or self.full(board) == True:
+            return self.evaluate(board, maximizing)
+
+        if maximizing:
+            maxEval = float('-inf')
+
+            for possibility in self.generate_possible_moves(board, player = "X"):
+                eval = self.minimax(possibility, depth - 1, alpha, beta, maximizing=False)
+                maxEval = max(eval, maxEval)
+                alpha = max(alpha, eval)
+
+                if beta <= alpha:
+                    break
+
+            return maxEval
+        
+        else:
+            minEval = float('inf')
+            
+            for possibility in self.generate_possible_moves(board, player = "O"):
+                eval = self.minimax(possibility, depth - 1, alpha, beta, maximizing=True)
+                minEval = min(eval, maxEval)
+                alpha = min(alpha, eval)
+
+                if beta <= alpha:
+                    break
+
+            return minEval
+
+            
 
