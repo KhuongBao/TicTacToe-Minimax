ee1eedac19922ce208f00c7866c75c030565a6ba
TheSuspecct <baogiakhuong2005@gmail.com>
2024-02-08 17:12:52 -0500

    Update game.py
    added player_move and play function
    
    modified evaluate and minimax function

--- game.py
+++ game.py
@@ -5,6 +5,15 @@ class TicTacToe:
     def __init__(self):
         self.board = np.full((3, 3), "-")
 
+    def player_move(self, board, move='X'):
+        while True:
+            position = input("Enter your move (row, col): ").split()
+            if int(position[0]) <= 3 and int(position[1]) <= 3:
+                if board[int(position[0]) - 1, int(position[1]) - 1] == '-':
+                    board[int(position[0]) - 1, int(position[1]) - 1] = move
+                    break
+
+
     def winner(self, board):
         for row in board:
             if np.all(row == row[0]) and row[0] != '-':
@@ -25,14 +34,15 @@ class TicTacToe:
     def full(self, board):
         return np.all(board != '-')
     
-    def evaluate(self, board, maximizing):
+    def evaluate(self, board, depth, maximizing):
         if self.winner(board):
             if maximizing == True:
-                return 1
-            return -1
+                return depth
+            return -depth
 
         elif self.full(board):
             return 0
+        return 0
 
     def generate_possible_moves(self, board, player):
         possible_moves = []
@@ -47,12 +57,13 @@ class TicTacToe:
 
     def minimax(self, board, depth, alpha, beta, maximizing):
         if depth == 0 or self.winner(board) == True or self.full(board) == True:
-            return self.evaluate(board, maximizing)
+            return self.evaluate(board, depth, maximizing)
 
         if maximizing:
             maxEval = float('-inf')
 
-            for possibility in self.generate_possible_moves(board, player = "X"):
+            for possibility in self.generate_possible_moves(board, player = "O"):
+                #print(possibility, self.winner(possibility), self.evaluate(possibility, depth, maximizing=True), end='\n\n')
                 eval = self.minimax(possibility, depth - 1, alpha, beta, maximizing=False)
                 maxEval = max(eval, maxEval)
                 alpha = max(alpha, eval)
@@ -65,15 +76,27 @@ class TicTacToe:
         else:
             minEval = float('inf')
             
-            for possibility in self.generate_possible_moves(board, player = "O"):
+            for possibility in self.generate_possible_moves(board, player = "X"):
+                #print(possibility, self.winner(possibility), self.evaluate(possibility, depth, maximizing=False), end='\n\n')
                 eval = self.minimax(possibility, depth - 1, alpha, beta, maximizing=True)
-                minEval = min(eval, maxEval)
+                minEval = min(eval, minEval)
                 alpha = min(alpha, eval)
 
                 if beta <= alpha:
                     break
 
             return minEval
+    
+    def play(self):
+        while self.winner(self.board) == False or self.full(self.board) == False:
+            print(self.board)
+            self.player_move(self.board)
+            com = self.minimax(self.board, 10, float("-inf"), float("inf"), True)
+            print(com)
+
+        print("Gameover!")
+
 
             
-
+game = TicTacToe()
+game.play()
